c45c334ba32f7c19ffa6e39b9dcc7c9e61cf082f
wepungsan <85734839+wepungsan@users.noreply.github.com>
2021-06-27 01:29:09 +0900

    2.1

--- js/todo.js
+++ js/todo.js
@@ -23,7 +23,7 @@ function showToDo(newToDoObj) {
     const span = document.createElement("span");
     span.innerText = newToDoObj.text;
     const button = document.createElement("button");
-    button.innerText = "❌";
+    button.innerText = "X";
     button.addEventListener("click", deleteToDo);
     li.appendChild(span);
     li.appendChild(button);
